bc02ff52d5ac1f8254303478cab9d7fd65ed044e
Hasnain Ahmad <hasnainahmadkhan04@gmail.com>
2024-02-05 19:50:26 +0500

    challange completed

---
+++ script.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "light-grey": "#f2f2f2",
+        "bright-orange": "#e38826",
+        "dark-cayn": "#006970",
+        "very-dark-cayn": "#004241",
+      },
+      fontFamily: {
+        Lexend: ["Lexend"],
+        BSD: ["BSD"],
+      },
+    },
+  },
+};
